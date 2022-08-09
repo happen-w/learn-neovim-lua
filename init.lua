b2f44653a325f14c3488c21fd1ace858f337f68a
happen <1592578957@qq.com>
2022-08-09 16:11:04 +0800

    remove plugins

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@ require("plugin-config.nvim-lspconfig")
 -- 插件配置
 require("plugin-config.nvim-tree")
 require("plugin-config.doom-one")
-require("plugin-config.mason")
+--require("plugin-config.mason")
 --require("plugin-config.bufferline")
 --require("plugin-config.lualine")
 --require("plugin-config.telescope")
